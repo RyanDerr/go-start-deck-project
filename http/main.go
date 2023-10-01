1396aba10b2dd4835f75d1c1a5a8f82c8d036c30
Ryan Derr <JamesRDerr@gmail.com>
2023-10-01 18:33:23 -0500

    Implement Sample Writer Interface

--- http/main.go
+++ http/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("http://google.com/")
@@ -14,8 +17,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	bs := make([]byte, 99999)
-	resp.Body.Read(bs)
+	lw := logWriter{}
 
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
 	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:Ruanderp1", len(bs))
+	return len(bs), nil
 }
